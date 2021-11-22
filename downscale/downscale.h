c5a0c033eff4243d2e21657a3e1b21f919c59ab2
Hchaimaa <58629186+Hchaimaa@users.noreply.github.com>
2021-11-22 20:40:25 +0100

    Update downscale.h

--- downscale/downscale.h
+++ downscale/downscale.h
@@ -4,7 +4,8 @@
 
 const int osize = 256; // 256 ipower 8 of 2 to make division easier 
 const int isize = 1024; // 1024 devided by osize else the image might be croped 
-
+#define DATA_WIDTH 8
+typedef ac_fixed<DATA_WIDTH,DATA_WIDTH,false,AC_RND_INF,AC_SAT> int_fixed;
 
 //down sizing an image using simple average methode. input:
 //		source is the source image to down size 
